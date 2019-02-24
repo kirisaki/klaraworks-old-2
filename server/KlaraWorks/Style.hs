14c2b1be162ee770d8452ec3bdae9d7d75d37050
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-02-24 18:50:02 +0900

    Define colors

--- server/KlaraWorks/Style.hs
+++ server/KlaraWorks/Style.hs
@@ -4,16 +4,35 @@ module KlaraWorks.Style
   , index
   ) where
 
-import           Clay                    hiding (style)
+import           Clay                    hiding (div, style)
 import qualified Data.ByteString.Lazy    as LBS
 import qualified Data.Text               as ST
 import qualified Data.Text.Lazy.Encoding as LTE
 import           Lucid
 
+rgb' :: Integer -> Color
+rgb' hex = rgb r g b
+  where
+    b = hex `mod` 0x0100
+    g = hex `mod` 0x010000 `div` 0x0100
+    r = hex `div` 0x010000
+
+kDark, kGray, kWhite, kPink,kPeach :: Color
+kDark = rgb' 0x444444
+kGray = rgb' 0x888888
+kWhite = rgb' 0xf0f0f0
+kPink = rgb' 0xfc2063
+kPeach = rgb' 0xfd5185
+
+
 style :: LBS.ByteString
-style = LTE.encodeUtf8 . renderWith compact [] $
+style = LTE.encodeUtf8 . renderWith compact [] $ do
+  body ? do
+    backgroundColor kDark
+    backgroundImage $ url "back.svg"
+    color white
   h1 ?
-  color "#fa0"
+    color "#fa0"
 
 index :: LBS.ByteString
 index =  renderBS $
