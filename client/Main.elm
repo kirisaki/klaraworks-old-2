8a16c4eb5514edd5110b1b6f405bfeb1b527f410
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-03-03 13:41:15 +0900

    Fetch detail of work

--- client/Main.elm
+++ client/Main.elm
@@ -55,6 +55,22 @@ type alias WorkSummary =
     , title : String
      }
 
+type Direction
+    = ToRight
+    | ToLeft
+
+type Contents
+    = Picture String
+    | Comic Direction (List String)
+
+type alias WorkDetail =
+    { id_ : String
+    , time : Time.Posix
+    , title : String
+    , origin : Maybe String
+    , contets : Contents
+     }
+
 init : () -> Url -> Key -> ( Model, Cmd Msg )
 init _ u k =
     ( { key = k
@@ -94,19 +110,23 @@ string16 =
 posix : BD.Decoder Time.Posix
 posix = BD.map (Time.millisToPosix << ((*) 1000)) <| BD.signedInt32 Bytes.BE
 
-fetchWorksList : Language -> Task Http.Error (List WorkSummary)
-fetchWorksList l =
+list16 : BD.Decoder a -> BD.Decoder (List a)
+list16 d =
     let
-        list d =
-            BD.unsignedInt16 Bytes.BE
-                |> BD.andThen (\len -> BD.loop (len, []) (listStep d))
-        listStep d (n, xs) =
+        listStep d1 (n, xs) =
             if n <= 0 then
                 BD.succeed (BD.Done xs)
             else
-                BD.map (\x -> BD.Loop (n - 1, x :: xs)) d
+                BD.map (\x -> BD.Loop (n - 1, x :: xs)) d1
+    in
+        BD.unsignedInt16 Bytes.BE
+            |> BD.andThen (\len -> BD.loop (len, []) (listStep d))
+
+fetchWorksList : Language -> Task Http.Error (List WorkSummary)
+fetchWorksList l =
+    let
         summary = BD.map3 WorkSummary string8 posix string16
-        decoder = apiDecoder <| list summary
+        decoder = apiDecoder <| list16 summary
     in
         Http.task
             { method = "GET"
@@ -116,6 +136,49 @@ fetchWorksList l =
             , resolver = bytesResolver decoder
             , timeout = Nothing
             }
+
+fetchWorkDetail : String -> Language -> Task Http.Error WorkDetail
+fetchWorkDetail i l =
+    let
+        origin =
+            string8
+                |> BD.andThen
+                   (\str ->
+                        case str of
+                            "" -> BD.succeed Nothing
+                            x -> BD.succeed <| Just x
+                   )
+        contents =
+            BD.unsignedInt8
+                |> BD.andThen
+                   (\t ->
+                       case t of
+                           0x01 ->
+                               BD.map Picture string8
+                           0x02 ->
+                               BD.map (Comic ToRight) (list16 string8)
+                           0x03 ->
+                               BD.map (Comic ToLeft) (list16 string8)
+                           _ -> BD.fail
+                   )
+        detail =
+            BD.map5 WorkDetail
+                string8
+                posix
+                string16
+                origin
+                contents
+        decoder = apiDecoder <| detail
+    in
+        Http.task
+            { method = "GET"
+            , headers = []
+            , url = "/api/works/" ++ i ++ "?" ++ languageTo3Code l
+            , body = Http.emptyBody
+            , resolver = bytesResolver decoder
+            , timeout = Nothing
+            }
+
 
 bytesResolver : BD.Decoder a -> Http.Resolver Http.Error a
 bytesResolver decoder =
